ce098650da2e0c3d9b07df5edb90e87b1dcfb47f
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-01 00:39:46 +0900

    Test코드 작성

---
+++ src/test/java/com/spdkwon/comunity/springboot/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.spdkwon.comunity.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class) // 테스트 진행 시 JUnit에 내장된 실행자가 아닌 SpringRunner라는 스프링 실행자를 사용
+@WebMvcTest(controllers = HelloController.class) // 컨트롤러만 사용하고 Web에 집중할 수 있는 테스트 어노테이션
+public class HelloControllerTest {
+
+    @Autowired // Spring이 관리하는 Bean을 주입받음
+    private MockMvc mvc; // 웹 API를 테스트할 때 사용하고 스프링 MVC 테스트의 시작점
+
+    @Test
+    public void hello() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청
+                .andExpect(status().isOk()) // mvc.perform의 결과가 200인지 아닌지 상태 검증
+                .andExpect(content().string(hello)); // mvc.perform의 결과검증
+    }
+}
