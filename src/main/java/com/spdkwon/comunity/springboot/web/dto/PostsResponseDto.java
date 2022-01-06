b1385478588b4126cc46f417cc6c6049c74c43bd
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-06 16:55:41 +0900

    Update API add...

---
+++ src/main/java/com/spdkwon/comunity/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.spdkwon.comunity.springboot.web.dto;
+
+import com.spdkwon.comunity.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
