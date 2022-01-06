ccccc8d0c4195f700042f67b6d3c168d714ca50d
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-06 16:55:17 +0900

    Update API add...

--- src/main/java/com/spdkwon/comunity/springboot/web/PostsApiController.java
+++ src/main/java/com/spdkwon/comunity/springboot/web/PostsApiController.java
@@ -1,11 +1,13 @@
 package com.spdkwon.comunity.springboot.web;
 
 import com.spdkwon.comunity.springboot.service.PostsService.PostsService;
+import com.spdkwon.comunity.springboot.web.dto.PostsResponseDto;
 import com.spdkwon.comunity.springboot.web.dto.PostsSaveRequestDto;
+import com.spdkwon.comunity.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +18,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
