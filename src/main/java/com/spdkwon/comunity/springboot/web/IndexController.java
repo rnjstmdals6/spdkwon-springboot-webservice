16b06da4c6508548e3a5064a08f93a3aa6bf713a
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-09 21:44:45 +0900

    Mustache Index add...

---
+++ src/main/java/com/spdkwon/comunity/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.spdkwon.comunity.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반활할 때 아퓨의 경로와 뒤의 파일 확장자는 자동으로 지정됨
+    {
+        return "index";
+    }
+}
