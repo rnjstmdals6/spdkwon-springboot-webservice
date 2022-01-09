d040dc4c873e31b044dff0083ca38327a8426518
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-09 22:54:37 +0900

    Add post update/delete screen

--- src/main/java/com/spdkwon/comunity/springboot/service/PostsService/PostsService.java
+++ src/main/java/com/spdkwon/comunity/springboot/service/PostsService/PostsService.java
@@ -2,13 +2,16 @@ package com.spdkwon.comunity.springboot.service.PostsService;
 
 import com.spdkwon.comunity.springboot.domain.posts.Posts;
 import com.spdkwon.comunity.springboot.domain.posts.PostsRepository;
+import com.spdkwon.comunity.springboot.web.dto.PostsListResponseDto;
 import com.spdkwon.comunity.springboot.web.dto.PostsResponseDto;
 import com.spdkwon.comunity.springboot.web.dto.PostsSaveRequestDto;
 import com.spdkwon.comunity.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +37,12 @@ public class PostsService {
                 -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) // 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것으로 조회 속도가 개선
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+        // .map(posts -> new PostsListResponseDto(posts)) 위와 동일한 코드
+        //
+    }
+
 }
--- src/main/java/com/spdkwon/comunity/springboot/web/IndexController.java
+++ src/main/java/com/spdkwon/comunity/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.spdkwon.comunity.springboot.web;
 
+import com.spdkwon.comunity.springboot.service.PostsService.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반활할 때 아퓨의 경로와 뒤의 파일 확장자는 자동으로 지정됨
-    {
+    public String index(Model model){ // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반활할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
