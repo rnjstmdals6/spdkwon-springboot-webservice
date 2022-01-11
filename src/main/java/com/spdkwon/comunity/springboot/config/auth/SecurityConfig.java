0c978b72f348fa0b5a34158f3b1f9a6eae48bd93
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-12 04:55:18 +0900

    구글 로그인 연동 2

--- src/main/java/com/spdkwon/comunity/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/spdkwon/comunity/springboot/config/auth/SecurityConfig.java
@@ -14,16 +14,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf.disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable
+        http.csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable
                 .and()
                 .authorizeRequests() // URL 별 권한 관리설정 옵션의 시작점
                 .antMatchers("/", "/css/**", "/images/**",
                         "/js/**", "/h2-console/**").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // 권한 관리 대상을 지정, permitAll()을 통해 전체 열람권한
-                .anyRequest().authenticated() // 설정된 값들 이외 나머지 URL들을 나타냄
+                .anyRequest() // 설정된 값들 이외 나머지 URL들을 나타냄
+                .authenticated() //authenticated()를 축해 나머지 URL들은 모두 인증된 사용자들에게만 허용
                 .and()
-                .logout().logoutSuccessUrl("/") // authenticated()를 축해 나머지 URL들은 모두 인증된 사용자들에게만 허용
+                .logout().logoutSuccessUrl("/") // 로그아웃 기능에 대한 설정의 진입점, 로그아웃 성공시 / 주소로 이동
                 .and().oauth2Login().userInfoEndpoint() // OAuth2 로그인 기능에 대한 여러 설정의 진입점
-                .userService(customOAuth2UserService);
+                .userService(customOAuth2UserService); // 소셜 로그인 성공시 후속 조치를 진행할 UserService 인터페이스 구현체를 등록
     }
 }
