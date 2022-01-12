08917f33bf137ef86f89aaeca2b372177459fc8b
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-13 03:29:31 +0900

    구글 로그인 연동 3

--- src/main/java/com/spdkwon/comunity/springboot/web/IndexController.java
+++ src/main/java/com/spdkwon/comunity/springboot/web/IndexController.java
@@ -1,23 +1,28 @@
 package com.spdkwon.comunity.springboot.web;
 
+import com.spdkwon.comunity.springboot.config.auth.dto.SessionUser;
 import com.spdkwon.comunity.springboot.service.PostsService.PostsService;
 import com.spdkwon.comunity.springboot.web.dto.PostsResponseDto;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){ // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반활할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         return "index";
     }
 
