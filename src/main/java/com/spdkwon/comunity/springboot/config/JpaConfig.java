a730b91cfb2239c3119c263157675a0727253eca
rnjstmdals6 <rnjstmdals6@naver.com>
2022-01-14 19:44:01 +0900

    Apply Test Security

---
+++ src/main/java/com/spdkwon/comunity/springboot/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.spdkwon.comunity.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
--- src/test/java/com/spdkwon/comunity/springboot/web/HelloControllerTest.java
+++ src/test/java/com/spdkwon/comunity/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.spdkwon.comunity.springboot.web;
 
+import com.spdkwon.comunity.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 테스트 진행 시 JUnit에 내장된 실행자가 아닌 SpringRunner라는 스프링 실행자를 사용
-@WebMvcTest(controllers = HelloController.class) // 컨트롤러만 사용하고 Web에 집중할 수 있는 테스트 어노테이션
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+}) // 컨트롤러만 사용하고 Web에 집중할 수 있는 테스트 어노테이션
 public class HelloControllerTest {
 
     @Autowired // Spring이 관리하는 Bean을 주입받음
     private MockMvc mvc; // 웹 API를 테스트할 때 사용하고 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello() throws Exception{
         String hello = "hello";
@@ -29,6 +36,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // mvc.perform의 결과검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto() throws Exception{
         String name = "hello";
